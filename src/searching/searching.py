cd0de650447e8f463e4393077e8bb4d3889ae1f0
Carlos Banks <cbanks@clypr.co>
2020-09-03 21:11:37 -0400

    completed searching test

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
+    low = 0
+    high = len(arr) - 1
+    while low < high:
+        middle = int((low + high) / 2)
+        if arr[middle] == target:
+            return middle
+        if arr[middle] < target:
+            low = middle
+        else: 
+            high = middle
 
     return -1  # not found
